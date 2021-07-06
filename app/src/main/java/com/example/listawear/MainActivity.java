66dbce22496e3177ed7c0b1422c65655817c738e
Hector Saldaña <hectorsalbe@hotmail.com>
2021-07-05 19:26:41 -0500

    actualizacion codigo

--- app/src/main/java/com/example/listawear/MainActivity.java
+++ app/src/main/java/com/example/listawear/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.listawear;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.wear.widget.WearableLinearLayoutManager;
 import androidx.wear.widget.WearableRecyclerView;
 
@@ -23,17 +25,43 @@ public class MainActivity extends Activity {
 
         WearableLinearLayoutManager layoutManager = new WearableLinearLayoutManager(this);
         layoutManager.setOrientation(WearableLinearLayoutManager.VERTICAL);
+        layoutManager.setLayoutCallback(new CustomScrollingLayoutCallback());
 
         recyclerView.setLayoutManager(layoutManager);
 
-        items.add(new ItemsList(R.mipmap.ic_launcher,"Flutter"));
-        items.add(new ItemsList(R.mipmap.ic_launcher,"PHP"));
-        items.add(new ItemsList(R.mipmap.ic_launcher,"JavaScript"));
+        items.add(new ItemsList(R.mipmap.flutter,"Flutter"));
+        items.add(new ItemsList(R.mipmap.java,"Java"));
+        items.add(new ItemsList(R.mipmap.cplus,"C#"));
+        items.add(new ItemsList(R.mipmap.php,"PHP"));
+        items.add(new ItemsList(R.mipmap.javascript,"JavaScript"));
 
 
         ListAdapter listAdapter = new ListAdapter(items);
 
         recyclerView.setAdapter(listAdapter);
 
+    }
+}
+
+class CustomScrollingLayoutCallback extends WearableLinearLayoutManager.LayoutCallback {
+    /** How much should we scale the icon at most. */
+    private static final float MAX_ICON_PROGRESS = 0.65f;
+
+    private float progressToCenter;
+
+    @Override
+    public void onLayoutFinished(View child, RecyclerView parent) {
+
+        // Figure out % progress from top to bottom
+        float centerOffset = ((float) child.getHeight() / 2.0f) / (float) parent.getHeight();
+        float yRelativeToCenterOffset = (child.getY() / parent.getHeight()) + centerOffset;
+
+        // Normalize for center
+        progressToCenter = Math.abs(0.5f - yRelativeToCenterOffset);
+        // Adjust to the maximum scale
+        progressToCenter = Math.min(progressToCenter, MAX_ICON_PROGRESS);
+
+        child.setScaleX(1 - progressToCenter);
+        child.setScaleY(1 - progressToCenter);
     }
 }
